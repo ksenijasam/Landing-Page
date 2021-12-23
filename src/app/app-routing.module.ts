5b6a99db6e19032816a7b656b72e87f2fea5c4d0
ksenijasam <ksenija2702@gmail.com>
2021-12-23 20:39:05 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainPageComponent, children: [
+      { path: '', component: TopBarComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    TopBarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
